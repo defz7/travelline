20f4b1db946f642d28235c57652f73248076f283
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-04-13 21:48:00 +0700

    Add landingpage

--- components/Home/landingpage.tsx
+++ components/Home/landingpage.tsx
@@ -3,10 +3,30 @@ import Link from "next/link";
 
 function Landingpage() {
     return(
-        <div className="h-screen relative bg-blue-100">
-            <p>
-                Hello
-            </p>
+        <div className="h-screen relative bg-cstmyellow">
+            <div className="h-full grid grid-cols-2 content-center py-24 px-32">
+                <div className="h-full gap-5 2xl:gap-8 content-center grid">
+                    <h1 className="text-5xl xl:text-7xl 2xl:text-7xl text-white font-poppinsbold leading-[4.5rem] xl:leading-[6rem] 2xl:leading-[6rem] font-bold">
+                        Life is a journey, not a destination
+                    </h1>
+                    <p className="text-[13px] xl:text-[16px] 2xl:text-xl text-white">
+                        Let's start your journey with us, your dream will come true
+                    </p>
+                    <Link href="/">
+                        <a className="flex justify-center w-64 cursor-pointer text-white border-white bg-cstmdarkbrown hover:bg-white hover:text-black py-2 rounded-2xl text-xl font-medium">
+                            Explore Destinations
+                        </a>
+					</Link>
+                </div>
+                <div className="mr-0 grid content-center m-auto">
+                    <Image
+                    src="/Images/Home/kisspng-travel-clip-art-portable-network-graphics-vector-g-viaggi-in-comitiva-faenza-ra-astorre-agenzia-viag-5be4f4439a98f7 1.svg"
+                    alt="Logo"
+                    width={910}
+                    height={745}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'landingpage': "url('/Images/Home/Rectangle 4.svg)",
+      },
+      colors: {
+        cstmyellow: "#F0C53E",
+        cstmdarkbrown: "#B25A59",
+        brownfont: "#7F2736",
+      },
+      fontFamily: {
+        
+      },
+    },
   },
   plugins: [],
 }
