ff0dfc6178642271f68fbf3317fc06eb9cee104d
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-15 23:35:43 +0700

    update testimony section and install heroicons

--- components/Home/testimony.tsx
+++ components/Home/testimony.tsx
@@ -1,16 +1,32 @@
+import { ArrowLongRightIcon } from '@heroicons/react/24/outline';
+
 function Testimony() {
     return(
         <div className="relative bg-white">
             <div className="grid w-full py-24 px-16 xl:px-32 2xl:px-52">
                 <div className="grid xl:grid-cols-2 gap-10">
-                    <div className="lg:max-w-lg 2xl:max-w-xl space-y-3">
+                    <div className="lg:max-w-lg 2xl:max-w-xl space-y-5">
                         <div className="flex items-center">
                             <div className="border border-[#2C2D32] w-[50px] h-0 mr-3"></div>
-                            <p className="font-nunito uppercase">Testimonial</p>
+                            <p className="font-nunito uppercase">
+                                Testimonial
+                            </p>
                         </div>
-                        <p className="text-3xl font-montserratBold">What Customers Say?</p>
-                        <p className="font-nunito text-base pt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                        <p className="text-3xl font-montserratBold">
+                            What Customers Say?
+                        </p>
+                        <p className="font-nunito text-base py-3">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </p>
+                        <button className="flex justify-between w-[220px] md:w-[240px] rounded-full border border-[#EE5060] cursor-pointer">
+                            <div className="grid content-center justify-items-center pl-8 pt-2 text-[#EE5060]">
+                                Write your thought
+                            </div>
+                            <div className="p-2 rounded-full border border-[#EE5060]">
+                                <ArrowLongRightIcon className="w-6 text-[#EE5060]"/>
+                            </div>
+                        </button>
                     </div>
                     <div className="grid content-center justify-items-center">
                         <div className="flex items-center p-8 pb-3 rounded-2xl shadow-lg bg-gray-100 max-w-xl">
