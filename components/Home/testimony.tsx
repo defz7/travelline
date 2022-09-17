50bf35cefd4321d63ee5ee9cd3d22e2a34594dd7
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-17 19:13:37 +0700

    fix: testimony section

--- components/Home/testimony.tsx
+++ components/Home/testimony.tsx
@@ -2,9 +2,9 @@ import { ArrowLongRightIcon } from '@heroicons/react/24/outline';
 
 function Testimony() {
     return(
-        <div className="relative bg-white">
-            <div className="grid w-full py-24 px-16 xl:px-32 2xl:px-52">
-                <div className="grid xl:grid-cols-2 gap-10">
+        <div className="w-full h-full bg-white">
+            <div className="grid py-24 px-16 xl:px-32 2xl:px-52">
+                <div className="grid xl:grid-cols-2 gap-10 xl:container xl:mx-auto">
                     <div className="lg:max-w-lg 2xl:max-w-xl space-y-5">
                         <div className="flex items-center">
                             <div className="border border-[#2C2D32] w-[50px] h-0 mr-3"></div>
@@ -31,7 +31,9 @@ function Testimony() {
                     <div className="grid content-center justify-items-center">
                         <div className="flex items-center p-8 pb-3 rounded-2xl shadow-lg bg-gray-100 max-w-xl">
                             <div className="grid content-center">
-                                <p className="font-nunitoBold max-w-xs">"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry."</p>
+                                <p className="font-nunitoBold max-w-xs">
+                                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                                </p>
                                 <div className="flex items-center justify-center">
                                     <img
                                         src="/Images/Home/Group 5436.svg"
@@ -39,8 +41,12 @@ function Testimony() {
                                         alt="profile picture"
                                     />
                                     <div className="grid">
-                                        <p className="font-nunitoSemi">Fay Nakila</p>
-                                        <p className="font-nunito text-slate-500">Traveller</p>
+                                        <p className="font-nunitoSemi">
+                                            Fay Nakila
+                                        </p>
+                                        <p className="font-nunito text-slate-500">
+                                            Traveller
+                                        </p>
                                     </div>
                                 </div>
                             </div>
