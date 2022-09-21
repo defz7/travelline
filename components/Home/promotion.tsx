e11291adf7b58533304e77aedd07281eda295c36
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-21 15:35:17 +0700

    fix: add responsiveness on promotion section

--- components/Home/promotion.tsx
+++ components/Home/promotion.tsx
@@ -4,28 +4,31 @@ function Promotion() {
     return(
         <div className="h-full grid bg-white pt-24 xl:py-24 px-16 xl:px-32 2xl:px-52">
             <div className="grid xl:container xl:mx-auto justify-items-center content-center space-y-10">
-                <h1 className="font-poppinsBold text-black text-2xl 2xl:text-3xl">
+                <h1 className="font-poppinsBold text-black text-2xl 2xl:text-3xl text-center">
                     <span className="text-brownfont">
                         Check Our Best
                     </span> {""}
                         Promotional Tour
                 </h1>
-                <div className="w-5/6 xl:w-3/4">
+                <div className="w-full lg:w-5/6 xl:w-3/4">
                     <p className="font-nunito items-center text-black text-base 2xl:text-xl text-center">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </p>
                 </div>
-                <div className="grid grid-cols-3">
+                <div className="grid grid-cols-1 md:grid-cols-3">
                     <Link href="/">
                         <a className="flex justify-center cursor-pointer ">
                             <img
                                 src="/Images/Home/Mask Group (4).svg"
-                                className="w-[450px]"
+                                className="sm:w-3/4 md:w-[450px]"
                             />
                         </a>
                     </Link>
+                    <p className="grid md:hidden pb-5 md:pb-0 justify-center text-brownfont font-poppinsMedium text-xl">
+                        Beijing
+                    </p>
                     <Link href="/">
                         <a className="flex justify-center cursor-pointer ">
                             <img
@@ -34,6 +37,9 @@ function Promotion() {
                             />
                         </a>
                     </Link>
+                    <p className="grid md:hidden pb-5 md:pb-0 justify-center text-brownfont font-poppinsMedium text-xl">
+                        Turkey
+                    </p>
                     <Link href="/">
                         <a className="flex justify-center cursor-pointer ">
                             <img
@@ -42,13 +48,18 @@ function Promotion() {
                             />
                         </a>
                     </Link>
-                    <p className="flex justify-center text-brownfont font-poppinsMedium text-xl">
+                    <p className="grid md:hidden justify-center text-brownfont font-poppinsMedium text-xl">
+                        Pakistan
+                    </p>
+
+                    {/* Text below for md screen and above */}
+                    <p className="hidden md:flex justify-center text-brownfont font-poppinsMedium text-xl">
                         Beijing
                     </p>
-                    <p className="flex justify-center text-brownfont font-poppinsMedium text-xl">
+                    <p className="hidden md:flex justify-center text-brownfont font-poppinsMedium text-xl">
                         Turkey
                     </p>
-                    <p className="flex justify-center text-brownfont font-poppinsMedium text-xl">
+                    <p className="hidden md:flex justify-center text-brownfont font-poppinsMedium text-xl">
                         Pakistan
                     </p>
                 </div>
