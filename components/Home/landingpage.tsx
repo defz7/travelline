0a087c4427bfe54015f004c7098454c0568816bb
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-22 19:34:50 +0700

    fix: update responsiveness on landingpage

--- components/Home/landingpage.tsx
+++ components/Home/landingpage.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 function Landingpage() {
     return(
-        <div className="w-full h-full px-7 sm:px-16 xl:px-32 2xl:px-52 py-32 xl:py-52 bg-cstmyellow">
+        <div className="w-full h-full px-7 sm:px-16 xl:px-32 2xl:px-52 py-24 sm:py-32 xl:py-52 bg-cstmyellow">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-4 content-center xl:container xl:mx-auto">
-                <div className="h-full gap-5 2xl:gap-11 content-center grid">
-                    <h1 className="font-poppinsBold text-4xl xl:text-5xl 2xl:text-7xl text-black leading-[3.5rem] xl:leading-[4rem] 2xl:leading-[6rem]">
+                <div className="h-full gap-3 2xl:gap-6 content-center grid">
+                    <h1 className="font-poppinsBold text-4xl xl:text-5xl 2xl:text-7xl text-black leading-[2.9rem] xl:leading-[4rem] 2xl:leading-[6rem]">
                         <span className="text-brownfont">
                             Life is a journey
                         </span>
@@ -15,7 +15,7 @@ function Landingpage() {
                     <p className="font-nunitoSemi text-[14px] xl:text-[16px] 2xl:text-2xl text-black">
                         Let's start your journey with us, your dream will come true
                     </p>
-                    <div className="flex gap-5">
+                    <div className="flex gap-5 2xl:pt-2">
                         <Link href="/">
                             <a className="font-nunitoSemi flex justify-center items-center self-center w-36 2xl:w-60 cursor-pointer text-white border-white bg-cstmdarkbrown hover:bg-cstmdarkerbrown hover:scale-110 duration-300 py-2 2xl:py-3 rounded-[80px] text-xs xl:text-xl font-medium">
                                 Explore Destinations
