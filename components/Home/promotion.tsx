ef34ce2972e9f5b8661dec3e24d50c09a3168cca
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-23 11:14:39 +0700

    feat: add aos on promotion section

--- components/Home/promotion.tsx
+++ components/Home/promotion.tsx
@@ -4,22 +4,22 @@ function Promotion() {
     return(
         <div className="h-full grid bg-white pt-14 md:pt-24 xl:py-24 px-7 sm:px-16 xl:px-32 2xl:px-52">
             <div className="grid xl:container xl:mx-auto justify-items-center content-center space-y-10">
-                <h1 className="font-poppinsBold text-black text-2xl 2xl:text-3xl text-center">
+                <h1 className="font-poppinsBold text-black text-2xl 2xl:text-3xl text-center" data-aos="fade-down" data-aos-duration="1500">
                     <span className="text-brownfont">
                         Check Our Best
                     </span> {""}
                         Promotional Tour
                 </h1>
                 <div className="w-full lg:w-5/6 xl:w-3/4">
-                    <p className="font-nunito items-center text-black text-base 2xl:text-xl text-center">
+                    <p className="font-nunito items-center text-black text-base 2xl:text-xl text-center" data-aos="fade-down" data-aos-duration="1500">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </p>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-3">
+                <div className="grid grid-cols-1 md:grid-cols-3" data-aos="fade-up" data-aos-duration="2000">
                     <Link href="/">
-                        <a className="flex justify-center cursor-pointer ">
+                        <a className="flex justify-center cursor-pointer">
                             <img
                                 src="/Images/Home/Mask Group (4).svg"
                                 className="sm:w-3/4 md:w-[450px]"
