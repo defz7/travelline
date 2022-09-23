b4f579e9aebb37db6bd87ec71d23d86d07ec5a88
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-09-23 11:17:49 +0700

    feat: add aos on globally section

--- components/Home/globally.tsx
+++ components/Home/globally.tsx
@@ -5,20 +5,20 @@ function Globally() {
     return(
         <div className="h-full bg-white py-24 px-16 xl:px-32 2xl:px-52">
             <div className="h-full grid justify-items-center content-center space-y-10 xl:container xl:mx-auto">
-                <h1 className="font-poppinsBold text-black text-2xl 2xl:text-3xl text-center">
+                <h1 className="font-poppinsBold text-black text-2xl 2xl:text-3xl text-center" data-aos="fade-down" data-aos-duration="1500">
                     <span className="font-bold text-brownfont">
                         Globally
                     </span> {""}
                         Accepted
                 </h1>
-                <div className="w-full lg:w-5/6 xl:w-3/4 justify-center">
+                <div className="w-full lg:w-5/6 xl:w-3/4 justify-center" data-aos="fade-down" data-aos-duration="1500">
                     <p className="font-nunito items-center text-black text-base 2xl:text-xl text-center">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </p>
                 </div>
-                <div className="flex justify-center">
+                <div className="flex justify-center" data-aos="fade-up" data-aos-duration="2000">
                     <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 md:gap-20 xl:gap-36">
                         <Link href="#!">
                             <a className="flex justify-center cursor-pointer w-full sm:w-3/4 md:w-full">
