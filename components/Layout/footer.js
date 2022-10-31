b9d830c864594c25a8a780e7b9dbdbfd36fcc3b2
Daffa K <daffa.kamal@mail.ugm.ac.id>
2022-10-31 17:00:37 +0700

    fix(Layout): tidy up codes and adjust a little bit

--- components/Layout/footer.js
+++ components/Layout/footer.js
@@ -1,16 +1,18 @@
-import Link from "next/link";
 
 function Footer() {
     return (
-        <div className="relative z-10 bg-[#A7283E] px-7 sm:px-16 py-20 xl:px-32 xl:py-32 2xl:pt-24 2xl:pb-24">
+        <footer className="relative z-10 bg-[#A7283E] px-7 sm:px-16 py-20 xl:px-32 xl:py-32 2xl:pt-24 2xl:pb-24">
             <div className="grid justify-items-center content-center">
                 <div className="flex justify-center items-center gap-5 sm:gap-10 pb-10">
                     <img
                         src="/Images/Home/Group 5421.svg"
-                        className="w-[120px] sm:w-[150px] md:w-[208px]"
+                        className="w-[120px] sm:w-[150px] md:w-[208px] cursor-pointer"
+                        alt="Lalasia logo"
                     />
                     <div className="font-montserratBold max-w-[156px] text-lg sm:text-xl text-white">
-                        <p>Domestic International Tours</p>
+                        <p>
+                            Domestic International Tours
+                        </p>
                     </div>
                 </div>
                 <div className="font-nunitoSemi grid justify-items-center">
@@ -20,29 +22,35 @@ function Footer() {
                     <div className="grid justify-items-center sm:flex justify-center items-center gap-5 pt-3">
                         <form>
                             <label className="block">
-                                <input className="px-4 py-1 rounded rounded-3xl border-2 border-[#F0C53E] placeholder-white text-white hover:border-white bg-[#A7283E]"/>
+                                <input className="px-4 py-1 rounded-3xl border-2 border-[#F0C53E] placeholder-white text-white hover:border-white bg-[#A7283E]"/>
                             </label>
                         </form>
-                        <button className="w-1/2 px-4 py-2 rounded rounded-3xl bg-[#F0C53E] hover:bg-[#E0B83B]">
+                        <button className="w-1/2 px-4 py-2 rounded-3xl bg-[#F0C53E] hover:bg-[#E0B83B]">
                             Subscribe
                         </button>
                     </div>
                 </div>
                 <div className="flex flex-wrap justify-center items-center gap-y-4 gap-x-6 pt-24">
                     <p className="font-nunito text-[#F0C53E] hover:text-white">
-                        <a href="#!">Careers</a>
+                        <a href="#!">
+                            Careers
+                        </a>
                     </p>
                     <div className="hidden sm:block border h-1/3 sm:h-full border-[#762432]"></div>
                     <p className="font-nunito text-[#F0C53E] hover:text-white">
-                        <a href="#!">Privacy Policy</a>
+                        <a href="#!">
+                            Privacy Policy
+                        </a>
                     </p>
                     <div className="hidden sm:block border h-1/3 sm:h-full border-[#762432]"></div>
                     <p className="font-nunito text-[#F0C53E] hover:text-white">
-                        <a href="#!">Terms & Conditions</a>
+                        <a href="#!">
+                            Terms & Conditions
+                        </a>
                     </p>
                 </div>
             </div>
-        </div>
+        </footer>
     );
 };
 export { Footer };
--- components/Layout/navbar.js
+++ components/Layout/navbar.js
@@ -1,53 +1,65 @@
 import Link from "next/link";
 
+const styleTextNav="font-poppinsRegular text-sm sm:text-base md:text-base xl:text-lg cursor-pointer text-black hover:text-white px-3 py-2"
+
 function Navbar() {
-    const styleTextNav="font-poppinsRegular text-sm sm:text-base md:text-base xl:text-lg cursor-pointer text-black hover:text-green px-3 py-2"
-	
 	return (
 		<div>
-			<nav className="w-full fixed bg-cstmyellow drop-shadow-lg px-7 sm:px-16 xl:px-32 2xl:px-52 lg:py-1 xl:py-0 z-30" data-aos="fade-down" data-aos-duration="1500">
+			<nav 
+				className="w-full fixed bg-cstmyellow drop-shadow-lg px-7 sm:px-16 xl:px-32 2xl:px-52 lg:py-1 xl:py-0 z-30" 
+				data-aos="fade-down" 
+				data-aos-duration="1500"
+			>
 				<div className="w-full xl:container xl:mx-auto">
 					<div className="w-full flex items-center h-9 md:h-12 lg:h-16 xl:h-20">
 						<div className="w-full flex items-center justify-between">
-							<div className="flex justify-center items-center" data-aos="fade-right" data-aos-duration="2000">
+							<div 
+								className="flex justify-center items-center" 
+								data-aos="fade-right" 
+								data-aos-duration="2000"
+							>
 								<Link href="/">
 									<img
 										loading="lazy"
 										src="/Images/Home/Group 5421 (1).svg"
-										className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[130px] xl:w-[150px] cursor-pointer"
+										className="w-[65px] md:w-[80px] lg:w-[100px] xl:w-[150px] cursor-pointer"
 									/>
 								</Link>
 							</div>
 							<div className="hidden lg:block" >
 								<div className="flex space-x-2 lg:space-x-6 2xl:space-x-11">
-									<Link href="#!">
+									<Link href="/">
                                         <a className={styleTextNav}>
-                                        Home
+                                        	Home
                                         </a>
 									</Link>
 									<Link href="#!">
                                         <a className={styleTextNav}>
-                                        Events
+                                        	Events
                                         </a>
 									</Link>
 									<Link href="#!">
                                         <a className={styleTextNav}>
-                                        Blog
+                                        	Blog
                                         </a>
 									</Link>
                                     <Link href="#!">
                                         <a className={styleTextNav}>
-                                        About
+                                        	About
                                         </a>
 									</Link>
 									<Link href="#!">
                                         <a className={styleTextNav}>
-                                        Contact
+                                        	Contact
                                         </a>
 									</Link>
 								</div>
 							</div>
-							<div className="flex justify-center items-center space-x-5 hidden lg:flex cursor-pointer" data-aos="fade-left" data-aos-duration="2000">
+							<div 
+								className="hidden lg:flex justify-center items-center space-x-5 cursor-pointer" 
+								data-aos="fade-left" 
+								data-aos-duration="2000"
+							>
 								<Link href="#!">
                                     <div className="flex justify-center items-center rounded-full bg-cstmdarkbrown hover:bg-cstmdarkerbrown hover:scale-110 duration-300 px-5 xl:px-8 py-3 gap-x-3 font-poppinsRegular text-white text-xs md:text-sm">
                                         <img
